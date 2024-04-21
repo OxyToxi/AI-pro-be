fe1178ff143d76019253869836080f029799ac10
OxyToxi <50832867+OxyToxi@users.noreply.github.com>
2024-04-22 04:27:56 +0900

    minor fix

--- src/test/java/team/service/AiProServiceTest.java
+++ src/test/java/team/service/AiProServiceTest.java
@@ -1,10 +1,12 @@
-package team.aiprobe.service;
+package team.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
+
+import team.aiprobe.service.AiProService;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
